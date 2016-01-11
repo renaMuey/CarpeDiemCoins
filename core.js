cc695a8d7008e09c505c7cf7cfdfa34f2f576bd6
Salah <salah.benyahya@mail.ru>
2016-01-11 18:48:56 +0000

    Update and rename CDB.js to core.js

---
+++ core.js
@@ -0,0 +1,32 @@
+// Carpe Diem Budget Javascript Code Here
+
+var transactions = [],
+    farestDate,
+    spendingPeriodLength,
+    totalRemainingMoney,
+    savingRate,
+    actualRemainingMoneyToSpend;
+
+
+function initialize() {
+    // setting the view variables values, to be executed at the begining and at every actualization
+}
+
+// here we call initialize()
+initialize();
+
+function newTransaction(title, commit, amout, date, status) {
+    // add a new action to the array transactions
+}
+
+function actualize() {
+    // compute the global vaiables when new data is available
+}
+
+function modifyTransaction(title, commit, amout, date, status) {
+    // modify an existing transaction
+}
+
+function configurize(farestDateEntered, spendingPeriodEntered, savingRateEntered) {
+    // encapsulate the configuration
+}
